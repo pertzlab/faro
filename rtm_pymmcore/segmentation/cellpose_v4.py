9ba3a0bd5b891e4fd4eab2b450446c53d5ce5ca3
Alex Landolt <alex.landolt@gmail.com>
2025-08-21 15:45:24 +0200

    Add cellposeV4 as a segmentation unit

---
+++ rtm_pymmcore/segmentation/cellpose_v4.py
@@ -0,0 +1,44 @@
+import numpy as np
+from rtm_pymmcore.segmentation.base_segmentation import Segmentator
+import skimage
+from cellpose import models
+
+
+class CellposeV4(Segmentator):
+
+    def __init__(
+        self,
+        custom_model_path=None,
+        diameter=None,
+        flow_threshold: float = 0.4,
+        cellprob_threshold: float = 0.0,
+        min_size: int = 50,
+    ):
+
+        self.diameter = diameter
+        self.flow_threshold = flow_threshold
+        self.cellprob_threshold = cellprob_threshold
+        self.min_size = min_size
+
+        if custom_model_path is None:
+            self.model = models.CellposeModel(gpu=True)
+        else:
+            self.model = models.CellposeModel(
+                pretrained_model=custom_model_path, gpu=True
+            )
+
+    def segment(self, img: np.ndarray) -> np.ndarray:
+
+        masks, flows, styles = self.model.eval(
+            img,
+            flow_threshold=self.flow_threshold,
+            cellprob_threshold=self.cellprob_threshold,
+            diameter=self.diameter,
+        )
+
+        if self.min_size > 0:
+            # remove cells below threshold
+            masks = skimage.morphology.remove_small_objects(
+                masks, min_size=self.min_size, connectivity=1
+            )
+        return masks
